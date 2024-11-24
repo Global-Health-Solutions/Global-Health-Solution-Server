61793d970ec27501a740d2e048267ac103da6a68
geek-abyte <osisegabriel@gmail.com>
2024-11-24 07:25:21 +0100

    changed chatbot API destination

--- routes/chatbotRoutes.js
+++ routes/chatbotRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const axios = require("axios");
 
 // const apiUrl = "https://diseaseprediction-rnbf.onrender.com/ask";
-const apiUrl = "https://aiconsultdemo.onrender.com/predict_disease";
+// const apiUrl = "https://aiconsultdemo.onrender.com/predict_disease";
+const apiUrl = "https://diseasepredictionsystem-3lfy.onrender.com/ask";
 
 router.post("/predict_disease", async (req, res) => {
   console.log("I'm Here!");
   try {
-    const { symptoms } = req.body;
-    const response = await axios.post(apiUrl, { symptoms });
+    const { question } = req.body;
+    const response = await axios.post(apiUrl, { question });
     res.json(response.data);
   } catch (error) {
     console.error("Error:", error);
